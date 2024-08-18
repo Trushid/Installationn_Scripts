7040db78c2da6abc57d1e14b3d46655a1e5093e9
Trushid Hatmode <130889499+Trushid@users.noreply.github.com>
2024-08-18 01:55:28 -0400

    Update Docker_install.sh

--- Docker_install.sh
+++ Docker_install.sh
@@ -6,13 +6,14 @@ sudo apt-get install apt-transport-https ca-certificates curl software-propertie
 
 Add Docker's Official GPG Key:-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-Set Up the Docker Stable Repository:-- echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+Set Up the Docker Stable Repository:-- echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] 
+                                       https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 UPDATE:- sudo apt-get update
 
- Install Docker: sudo apt-get install docker-ce docker-ce-cli containerd.io
+ Install Docker: sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
-Start and Enable Docker:-- sudo systemctl start docker  &   sudo systemctl enable docker
+Start and Enable Docker:-- sudo systemctl start docker  &&   sudo systemctl enable docker
 
  Fix the Permission Denied Error:-- sudo usermod -aG docker $USER
 
